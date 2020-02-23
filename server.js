944d5b545be92effd1449ab063bd769c2fa41120
Red Skull <bradfordmichael7@gmail.com>
2020-02-23 13:59:01 -0500

    attempting fix for heroku crash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 
-let PORT = process.env.port || 8080;
+let PORT = process.env.PORT || 8080;
 
 let app = express();
 
